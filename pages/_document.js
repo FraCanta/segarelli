ce2e00f95a5dbec0a3aa7e5d3ee271971b6038f5
FraCanta <fcantale14@gmail.com>
2026-05-08 08:08:57 +0200

    aggiornamento

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,18 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="application-name" content="Agriturismo Segarelli" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Segarelli" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#f8f6ec" />
         <meta
           name="google-site-verification"
           content="KK-7YRppi1kx9fCtpgOEXwURgpMC8VafdcwJDlHtPyY"
         />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <body className="antialiased">
         <Main />
